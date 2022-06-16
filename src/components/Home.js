47ac5f59c7b66175865ada8aeece6d4915602c66
imAryan24 <95496583+imAryan24@users.noreply.github.com>
2022-06-17 02:16:06 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet </p>
+         </div>
+    )
+}
+export default Home
