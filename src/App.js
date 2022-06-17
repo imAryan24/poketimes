432a35c20ad1bb0c8b3b01709e583059aa981014
imAryan24 <95496583+imAryan24@users.noreply.github.com>
2022-06-17 12:47:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/about' component={About} />
+         <Route exact path='/contact' component={Contact} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
