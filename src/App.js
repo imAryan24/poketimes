3781be5b65bbd614552f5ea7629fdb2637bc91d8
imAryan24 <95496583+imAryan24@users.noreply.github.com>
2022-06-18 14:31:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -16,7 +16,7 @@ class App extends Component {
           <Route exact path='/' element={<Home/>} />
           <Route exact path='/about' element={<About/>} />
             <Route exact path='/contact' element={<Contact />} />
-            <Route exact path='/:post_id' element={<Post/>} />
+            <Route path="/Post/: post_id" element={<Post/>}/>
           </Routes>
         </div>
       </Router>
